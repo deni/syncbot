d1bf3a7354af481edd5c828f3e709491d06bdbce
Denis Smajlović <deni@deni.dk>
2025-01-23 12:51:44 -0800

    Update with newest version from server

--- admin.py
+++ admin.py
@@ -5,11 +5,17 @@ from .models import Settings, Project, Page, Group
 # Register your models here.
 admin.site.register(Settings)
 admin.site.register(Project)
-admin.site.register(Page)
 
 class GroupAdmin(admin.ModelAdmin):
 	filter_horizontal = ['pages']
+	list_display = ['page_names', 'projects']
+	search_fields = ['pages__page_id', 'pages__project__domain', 'pages__revision', 'pages__name']
+
+class PageAdmin(admin.ModelAdmin):
+	readonly_fields = ['name']
+	list_display = ['name', 'project']
+	search_fields = ['name', 'project__domain', 'page_id', 'revision']
+	list_filter = ['project']
 
 admin.site.register(Group, GroupAdmin)
-
-
+admin.site.register(Page, PageAdmin)
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 
@@ -19,6 +21,7 @@ class Project(models.Model):
 
 class Page(models.Model):
 	name = models.CharField(max_length=200)
+	page_id = models.PositiveBigIntegerField()
 	project = models.ForeignKey(Project, on_delete=models.CASCADE)
 	revision = models.PositiveBigIntegerField()
 	
@@ -35,3 +38,31 @@ class Page(models.Model):
 class Group(models.Model):
 	pages = models.ManyToManyField(Page)
 	current = models.BooleanField()
+
+	def page_names(self):
+		page_names = []
+		for page in self.pages.all():
+			if not page.name in page_names:
+				page_names.append(page.name)
+
+		return( format_html_join(
+				mark_safe('<br>'),
+				'{}',
+				( (line,) for line in page_names )
+			)
+		)
+
+
+	def projects(self):
+		project_domains = []
+		for page in self.pages.all():
+			if not page.project.domain in project_domains:
+				project_domains.append(page.project.domain)
+		
+		return(
+			format_html_join(
+				mark_safe('<br>'),
+				'{}',
+				( (line,) for line in project_domains )
+			)
+		)
