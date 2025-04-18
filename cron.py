48f1e6ba57f96c4686e4182044e6f8e77276d37e
Laura Hausmann <laura@hausmann.dev>
2025-04-18 12:59:19 +0200

    Add cron tasks

--- cron.py
+++ cron.py
@@ -2,6 +2,10 @@ from threading import Thread
 import time
 import random
 import traceback
+import logging
+from syncbot.management.libraries import syncbot
+
+logger = logging.getLogger(__name__)
 
 def loop(interval, func):
 	time.sleep(random.randint(5, 60)) # offset / jitter
@@ -18,8 +22,16 @@ def schedule(interval, func):
 	t.daemon = True
 	t.start()
 
-def helloWorld():
-	print('Hello world', flush=True)
+def fetch():
+	logger.info("Running syncbot.fetch...")
+	syncbot.fetch()
+	logger.info("Finished running syncbot.fetch.")
+
+def synchronize():
+	logger.info("Running syncbot.synchronize...")
+	syncbot.synchronize()
+	logger.info("Finished running syncbot.synchronize.")
 
 def runCronTasks():
-	schedule(10, helloWorld)
+	#schedule(10, fetch)
+	#schedule(10, synchronize)
